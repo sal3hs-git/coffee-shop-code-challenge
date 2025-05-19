25c2658487ff18c60af4b6862bed9939b03cdaf7
Salehe <salehe.mkomwa@student.moringaschool.com>
2025-05-19 19:52:19 +0300

    added-coffee.py

---
+++ pipefile/coffee.py
@@ -0,0 +1,11 @@
+class coffee
+def __init__(self,name)
+self.name = name 
+   if not isintance (name,str):
+    raise TypeError("Name must be a string")   
+   if len(name) <3:
+    raise ValueError ("Name must be at leaast 3 characters long.")
+    self._name = name 
+
+    def name(self):
+        return self._name 
